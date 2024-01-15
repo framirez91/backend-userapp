9887a12a7a8f986580e5e5a5d4789aa06ec08612
framirez91 <framirez.d85@gmail.com>
2024-01-15 18:00:01 -0300

    Add custom query method and update user details service

--- src/main/java/com/felipe/backenduserapp/repositories/UserRepository.java
+++ src/main/java/com/felipe/backenduserapp/repositories/UserRepository.java
@@ -1,9 +1,18 @@
 package com.felipe.backenduserapp.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.felipe.backenduserapp.models.entities.User;
 
 public interface UserRepository
         extends CrudRepository<User, Long> {
+
+                Optional<User> findByUsername(String username);
+                
+                @Query("SELECT u FROM User u WHERE u.username = ?1")//sirve para hacer consultas personalizadas ?1 es el primer parametro
+                Optional<User> getUserByUsername(String username);
+
 
 }
--- src/main/java/com/felipe/backenduserapp/services/JpaUserDetailsService.java
+++ src/main/java/com/felipe/backenduserapp/services/JpaUserDetailsService.java
@@ -2,7 +2,9 @@ package com.felipe.backenduserapp.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -10,21 +12,35 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.felipe.backenduserapp.repositories.UserRepository;
+
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
+    @Autowired  
+    private UserRepository repository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("admin")) {
+        Optional<com.felipe.backenduserapp.models.entities.User> o = repository.findByUsername(username);
+
+        if (!o.isPresent()) {
             throw new UsernameNotFoundException(String.format("%s no fue encontrado",username));
         }
+
+        com.felipe.backenduserapp.models.entities.User user = o.get();
+
+
+
        List<GrantedAuthority> authorities = new ArrayList<>();
          authorities.add(new SimpleGrantedAuthority("ROLE_USER"));  
 
         return new User
-        (username,
-        "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        (user.getUsername(),
+        user.getPassword(),
         true,
         true,
         true,
